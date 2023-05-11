c5960c090cbf6ec0fe241eadd97c2034d7aecca2
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-11 21:48:57 +0100

    spec file updated

---
+++ spec/counter_spec.rb
@@ -0,0 +1,8 @@
+require 'counter'
+
+RSpec describe Counter do # code fine 
+  it "count to a number" do # code fine
+  result = #finish code here
+  expect(result).to eq "" #finish code here 
+  end
+end
