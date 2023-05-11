08c16a02770489bfb7bc79dabe7b6811dff28d28
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-11 13:46:35 +0100

    updated spec file

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,3 +1,9 @@
 require "check_codeword" #require the file first 
 
 RSpec.describe "check_codeword" do
+  it "check_codeword" do
+    codeword = "horse"   #given
+    result = check_codeword(codeword)
+      expect(result).to eq "Correct! Come in."
+  end 
+end 
