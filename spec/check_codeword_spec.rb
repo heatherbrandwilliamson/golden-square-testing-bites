43ef8f7d78b34b63299a334ce8080d7935644869
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-11 14:04:39 +0100

    changes to spec

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,9 +1,20 @@
 require "check_codeword" #require the file first 
 
 RSpec.describe "check_codeword" do
-  it "check_codeword" do
+  it "check codeword passed" do
     codeword = "horse"   #given
     result = check_codeword(codeword)
       expect(result).to eq "Correct! Come in."
   end 
+  it "check codeword wrong" do
+    codeword = "help"   #given
+    result = check_codeword(codeword)
+      expect(result).to eq "WRONG!"
+  end 
+  it "check codeword close" do
+    codeword = "helpme"   #given
+    result = check_codeword(codeword)
+      expect(result).to eq "Close, but nope."
+  end 
+
 end 
