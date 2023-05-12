046653508af9728d32e1bb5a5aea43f8289638e2
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-12 09:01:24 +0100

    finalised spec

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,8 +1,10 @@
 require 'counter'
 
-RSpec describe Counter do # code fine 
-  it "count to a number" do # code fine
-  result = #finish code here
-  expect(result).to eq "" #finish code here 
+RSpec.describe Counter do # error here 
+  it "count to a number" do 
+  counter1 = Counter.new() 
+  counter1.add(6)
+  result = counter1.report # is this correct?
+    expect(result).to eq "Counted to 6 so far." 
   end
 end
