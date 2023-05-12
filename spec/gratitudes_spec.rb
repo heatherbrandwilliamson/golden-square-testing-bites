f913a7dfafc8a225ccf642fd1ce6b6e24ca62dc5
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-12 10:37:50 +0100

    spec file created

---
+++ spec/gratitudes_spec.rb
@@ -0,0 +1,12 @@
+require 'gratitudes' #call the ruby file 
+
+RSpec.describe Gratitudes do #call the classname 
+    it "description" do #update description 
+        # call the methods here 
+    result = # call return method here 
+        expect(result).to eq #add code here 
+    end 
+
+    #additional methods here 
+end 
+
