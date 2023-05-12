a18dc70cc91888f816cfb0d4f3a946981e4d1159
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-12 13:20:02 +0100

    updated spec

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,10 +1,18 @@
 require 'present'    #call file name 
 
 RSpec.describe Present do # call the class here 
-    context "when no contents has been unwrapped" do
+    context "A contents has already been wrapped." do
+        it "fails" do 
+          present1 = Present.new()
+          present1.wrap("bike")
+            expect { present1.wrap("book") }.to raise_error "A contents has already been wrapped." # amend
+        end
+      end 
+
+    context "No contents have been wrapped." do
       it "fails" do 
         present1 = Present.new()
-        present1.wrap("book")
+        #present1.wrap("book")
           expect { present1.unwrap }.to raise_error "No contents have been wrapped." # amend
       end
     end 
